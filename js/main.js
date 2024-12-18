cb74f64047147f30152f4ef5fe215cc7b01580cf
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2024-12-18 15:37:05 -0800

    submetendo o formulario para criar um novo dado e aplicando funcionalidade do botao cancelar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos();
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+
+    try {
+        await api.salvarPensamento({ conteudo, autoria })
+        ui.renderizarPensamentos()
+    } catch {
+        alert("Erro ao salvar pensamento")
+    }
+}
+
+const botaoCancelar = document.getElementById("botao-cancelar");
+
+botaoCancelar.addEventListener("click", limparAreaDeTexto);
+
+function limparAreaDeTexto() {
+    const conteudo = document.getElementById("pensamento-conteudo")
+    const autoria = document.getElementById("pensamento-autoria")
+
+    conteudo.value = ""
+    autoria.value = ""
+}
