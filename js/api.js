4fc2cdebfbb5567928cf5a5be536b7c1d6aafd75
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-01-11 18:46:50 -0800

    Alterando dados com o PUT

--- js/api.js
+++ js/api.js
@@ -21,10 +21,39 @@ const api = {
 
             return await response.json();
         } catch (error){
-            alert('Erro ao buscar pensamentos')
+            alert('Erro ao salvar pensamentos')
             throw error
         }
-    }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`)
+            return await response.json()
+        } catch (error){
+            alert('Erro ao buscar pensamento')
+            throw error
+        }
+    },
+
+    async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+
+            return await response.json();
+        } catch (error){
+            alert('Erro ao editar pensamento')
+            throw error
+        }
+    },
 }
+
+
 
 export default api;
